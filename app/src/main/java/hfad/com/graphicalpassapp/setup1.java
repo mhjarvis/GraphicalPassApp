f9cd8f6c45ef1bd35a42657abb60664a27bdef0e
Markus <markus.h.jarvis@gmail.com>
2019-04-10 17:01:55 -0600

    *working version 04/10/19 *implemented shared preferences for storage

--- app/src/main/java/hfad/com/graphicalpassapp/setup1.java
+++ app/src/main/java/hfad/com/graphicalpassapp/setup1.java
@@ -7,11 +7,10 @@ import android.view.View;
 import android.widget.TextView;
 import android.content.Intent;
 
-import org.w3c.dom.Text;
-
 public class setup1 extends Activity {
 
-    public static final String EXTRA_MESSAGE = "loginText";
+    public static final String EXTRA_MESSAGE = "setupText1";
+    public static final String MY_PREFS_NAME = "Password_Part_1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +18,12 @@ public class setup1 extends Activity {
         setContentView(R.layout.activity_setup1);
     }
 
-    //action to go to setup screen 2
     public void getSetupPage_2(View view){
         Intent goToSetupScreen2 = new Intent(this, setup2.class);
         startActivity(goToSetupScreen2);
     }
 
-    //declare static string for password part 1
-    public static final String MY_PREFS_NAME = "Password_Part_1";
-
-
-    /* FUNCTION TO SAVE PASSWORD PART 1 TO SHARED PREFERENCES */
+    /******* FUNCTION TO SAVE PASSWORD PART 1 TO SHARED PREFERENCES *******/
     public void save_Password_Part_1(String value){
         SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
@@ -38,36 +32,62 @@ public class setup1 extends Activity {
         editor.apply(); //commit changes
     }
 
-    /* FUNCTION TO RETRIEVE STORED VALUE (for testing here) */
-    public String printName() {
+    /******* FUNCTION TO RETRIEVE STORED VALUE (for testing here) *******/
+    public void printName() {
         SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
-        String phrase=pref.getString("password_key_part_1", null); //variable 'phrase' holds value of key_name5
-        return phrase;  //return value to 'useElephant()'
-
+        //return pref.getString("password_key_part_1", null);
+        String returnKey = pref.getString("password_key_part_1", null);
+        TextView total = (TextView) findViewById(R.id.textViewName);
+        total.setText(returnKey); //call printName function to retrieve values
     }
 
     public void useBunny(View view){
         save_Password_Part_1("bunny");
-
-        TextView total = (TextView) findViewById(R.id.textViewName);
-        total.setText(printName()); //call printName function to retrieve values
-
+        printName();
     }
-    //execute when clicking on elephant image
+
     public void useElephant(View view){
         save_Password_Part_1("elephant");
+        printName();
+    }
+
+    public void useFish(View view){
+        save_Password_Part_1("fish");
+        printName();
+    }
+
+    public void useZebra(View view){
+        save_Password_Part_1("zebra");
+        printName();
+    }
+
+    public void useLion(View view){
+        save_Password_Part_1("lion");
+        printName();
+    }
+
+    public void useHawk(View view){
+        save_Password_Part_1("hawk");
+        printName();
+    }
+
+    public void useScorpion(View view){
+        save_Password_Part_1("scorpion");
+        printName();
+    }
+
+    public void useHippo(View view){
+        save_Password_Part_1("hippo");
+        printName();
+    }
+}
 
         /*
         SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
         editor.putString("key_name5", "elephant"); //save "elephant" to key_name5
-        editor.apply(); //commit changes */
+        editor.apply(); //commit changes
 
-        //testing - prints value to setup screen 1
+        testing - prints value to setup screen 1
         TextView total = (TextView) findViewById(R.id.textViewName);
-        total.setText(printName()); //call printName function to retrieve values
-    }
-
-
-
-}
+        total.setText(printName()); //call printName function to retrieve values */
--- app/src/main/java/hfad/com/graphicalpassapp/storage.java
+++ app/src/main/java/hfad/com/graphicalpassapp/storage.java
@@ -1,7 +1,10 @@
 package hfad.com.graphicalpassapp;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class storage extends AppCompatActivity {
 
@@ -9,5 +12,38 @@ public class storage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_storage);
+
+        useBunny();
+        useBunny2();
+    }
+
+    public void useBunny() {
+        TextView total = (TextView) findViewById(R.id.passwordpart1);
+        total.setText(printName()); //call printName function to retrieve values
+    }
+
+    /* FUNCTION TO RETRIEVE STORED VALUE (for testing here) */
+    public String printName() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);  //start shared preferences
+        SharedPreferences.Editor editor = pref.edit();
+        String phrase=pref.getString("password_key_part_1", null); //variable 'phrase' holds value of key_name5
+        String pass1intro = "Password Part 1: ";
+        phrase = pass1intro + phrase;
+        return phrase;  //return value to 'useElephant()'
+
+    }
+
+    public void useBunny2() {
+        TextView total = (TextView) findViewById(R.id.passwordpart2);
+        total.setText(printName2()); //call printName function to retrieve values
+    }
+    public String printName2() {
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);  //start shared preferences
+        SharedPreferences.Editor editor = pref.edit();
+        String phrase=pref.getString("password_key_part_2", null); //variable 'phrase' holds value of key_name5
+        String pass1intro = "Password Part 2: ";
+        phrase = pass1intro + phrase;
+        return phrase;  //return value to 'useElephant()'
+
     }
 }
